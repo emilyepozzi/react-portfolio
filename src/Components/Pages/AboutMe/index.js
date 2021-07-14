43f8bf8ded6348747ec8e62888f7b7449a01a9ed
Emily Pozzi <epozzi5@yahoo.com>
2021-07-14 13:49:12 -0500

    fixing css

--- src/Components/Pages/AboutMe/index.js
+++ src/Components/Pages/AboutMe/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Jumbotron, Row, Col } from 'reactstrap';
-import "./styles.css";
+import "./style.css";
 import skills from "../../Info/skills.js";
 
 class About extends React.Component {
